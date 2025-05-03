ce1024ee42c185887f572819e6650edf38a4758a
keshan8000 <124331848+keshan8000@users.noreply.github.com>
2025-05-03 14:29:38 +0530

    fitness app
    using resoponsivenes with flutter

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# responsive_app
-using responsiveness in flutter
+![DP Education IT Campus](https://dpeducation.lk/en/assets/images/brands/en/it-campus.png)
+
+# Fitness Tracker - Application 06 ⚡️
+
+A Flutter project that helps you track your Fitness effortlessly. This project is part of the Flutter Development Program by DP Education IT Campus. And focused deeply on responsive design.
+
+## Features
+
+- **Flutter Web**: Responsive web design, ensuring a seamless user experience across devices.
+- **Responsive**: Adapts to different screen sizes for optimal performance.
+- **Dark Mode**: A sleek and stylish dark mode option for users who prefer it.
+
+## Getting Started
+
+1. **Clone the repository**:
+
+   ```bash
+   git clone https://github.com/HGSChandeepa/Fitness-Tracker-App--06---DP-Education
+   ```
+
+2. **Navigate to the project directory**:
+
+   ```bash
+   cd Fitness-Tracker-App--06---DP-Education
+   ```
+
+3. **Install dependencies**:
+
+   ```bash
+   flutter pub get
+   ```
+
+4. **Run the app**:
+
+   ```bash
+   flutter run
+   ```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## What you will create
+
+![Finished App](https://github.com/HGSChandeepa/Fitness-Tracker-App--06---DP-Education/blob/main/assets/images/app.png)
---
+++ lib/widgets/line_chart.dart
@@ -0,0 +1,131 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive/constants/colors.dart';
+import 'package:responsive/data/line_chart_data.dart';
+import 'package:responsive/widgets/custum_card.dart';
+
+class LineChartCard extends StatelessWidget {
+  const LineChartCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final data = LineData();
+
+    return CustomCard(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Steps Overview",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              color: greyColor,
+            ),
+          ),
+          const SizedBox(height: 20),
+          AspectRatio(
+            aspectRatio: 16 / 6,
+            child: LineChart(
+              LineChartData(
+                //handles the touch behaviour
+                lineTouchData: const LineTouchData(
+                  handleBuiltInTouches: true,
+                ),
+                // gridData: const FlGridData(show: false),
+
+                //for the titles in the graph
+                titlesData: FlTitlesData(
+                  //remove the right titles
+                  rightTitles: const AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
+                  topTitles: const AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
+                  bottomTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      getTitlesWidget: (double value, TitleMeta meta) {
+                        //herer the value is a double we have to convet to int use toInt()
+                        return data.bottomTitle[value.toInt()] != null
+                            ? SideTitleWidget(
+                                axisSide: meta.axisSide,
+                                child: Text(
+                                  data.bottomTitle[value.toInt()].toString(),
+                                  style: const TextStyle(
+                                    fontSize: 12,
+                                    color: greyColor,
+                                  ),
+                                ),
+                              )
+                            : const SizedBox();
+                      },
+                    ),
+                  ),
+                  leftTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      interval: 1,
+
+                      //separation between the graph data and  the titiles
+                      reservedSize: 40,
+                      getTitlesWidget: (double value, TitleMeta meta) {
+                        return data.leftTitle[value.toInt()] != null
+                            ? Text(
+                                data.leftTitle[value.toInt()].toString(),
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  color: greyColor,
+                                ),
+                              )
+                            : const SizedBox();
+                      },
+                    ),
+                  ),
+                ),
+
+                //this will remove the border
+                borderData: FlBorderData(show: false),
+
+                // this will draw the graph with the provided data
+                lineBarsData: [
+                  LineChartBarData(
+                    color: selectionColor,
+
+                    //width of the bar
+                    barWidth: 3,
+
+                    //below the bar we can add a gradient
+                    belowBarData: BarAreaData(
+                      show: true,
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          // ignore: deprecated_member_use
+                          selectionColor.withOpacity(0.5),
+                          Colors.transparent
+                        ],
+                      ),
+                    ),
+
+                    // this will show the dot when the user hovered on the line graph
+                    dotData: const FlDotData(show: false),
+                    spots: data.spots,
+                  )
+                ],
+
+                // how to draw the graph on the xy cartecean plane
+                minX: 0,
+                maxX: 120,
+                maxY: 105,
+                minY: -5,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
